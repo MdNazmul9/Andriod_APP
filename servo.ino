5c65d0b86c9dae3e3ebb718c7f5d2c2d66c74303
Md. Nazmul Hossain <30841411+MdNazmul9@users.noreply.github.com>
2018-04-18 13:00:35 +0600

    Add files via upload

---
+++ servo.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo myservo;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(9);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  myservo.writeMicroseconds(800);
+  delay(1000);
+   myservo.writeMicroseconds(1500);
+   delay(1000);
+   myservo.writeMicroseconds(2000);
+   delay(2000);
+
+}
